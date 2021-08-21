0c650321d61d1f3ff442de01f35b42aa500dee8d
Marcelo Melo <marcelomelo.ciandt@sulamerica.com.br>
2021-08-21 19:00:18 -0300

    Add global style and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello again, React! =D</h1>
+      <GlobalStyle />
     </div>
   );
 }
